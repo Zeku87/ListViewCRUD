3fec9c9aa92e1eb0ae2140c4c18eb7aeae175a63
zeku87 <nocomment182@gmail.com>
2018-08-04 14:45:44 +0200

    Layout set for deletion process

--- app/src/main/java/com/joseezequielgallardo/listviewcrud/Adapter.java
+++ app/src/main/java/com/joseezequielgallardo/listviewcrud/Adapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,6 +14,9 @@ public class Adapter extends BaseAdapter {
 
     private ArrayList<String> items;
     private Context context;
+
+    //Determine wether the list is in delete mode or not
+    private boolean deletable = false;
 
     public Adapter(ArrayList<String> items, Context context){
         this.items = items;
@@ -43,8 +47,28 @@ public class Adapter extends BaseAdapter {
         }
 
         final TextView itemTextView = view.findViewById(R.id.item_text_view);
+        final CheckBox checkBox = view.findViewById(R.id.item_select_check_box);
         itemTextView.setText(this.items.get(pos));
 
+        if(isDeletable()){
+            checkBox.setVisibility(View.VISIBLE);
+            itemTextView.setVisibility(View.GONE);
+            checkBox.setText(this.items.get(pos));
+        }
+
+        if(!isDeletable()){
+            checkBox.setVisibility(View.GONE);
+            itemTextView.setVisibility(View.VISIBLE);
+        }
+
         return view;
+    }
+
+    public void setDeletable(boolean deletable){
+        this.deletable = deletable;
+    }
+
+    public boolean isDeletable(){
+        return this.deletable;
     }
 }
--- app/src/main/java/com/joseezequielgallardo/listviewcrud/MainActivity.java
+++ app/src/main/java/com/joseezequielgallardo/listviewcrud/MainActivity.java
@@ -3,11 +3,11 @@ package com.joseezequielgallardo.listviewcrud;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,22 +17,39 @@ public class MainActivity extends AppCompatActivity implements UpdateDialogFragm
     public ArrayList<String> items = new ArrayList<>();
     private Adapter adapter;
 
+    LinearLayout deleteButtonsLayout, addItemLayout;
+    Button acceptDeletionButton, cancelDeletionButton;
+
+    Button addItemButton;
+    EditText itemEditText;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button newButton = this.findViewById(R.id.new_btn);
-        final EditText itemEditText = this.findViewById(R.id.item_edit_text);
-        final ListView listView = this.findViewById(R.id.list_view);
+        /* Delete buttons layout*/
+        deleteButtonsLayout = this.findViewById(R.id.delete_buttons_layout);
+        acceptDeletionButton = this.findViewById(R.id.accept_delete_button);
+        cancelDeletionButton = this.findViewById(R.id.cancel_delete_button);
+
+        /* Ordinary layout */
+        addItemLayout = this.findViewById(R.id.add_item_layout);
+        addItemButton = this.findViewById(R.id.add_item_button);
+        itemEditText = this.findViewById(R.id.item_edit_text);
+        listView = this.findViewById(R.id.list_view);
+
         adapter = new Adapter(items, MainActivity.this);
         listView.setAdapter(adapter);
 
-        newButton.setOnClickListener(new View.OnClickListener() {
+        addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String item = itemEditText.getText().toString();
                 if(!item.isEmpty()){
+                    String emptyString = "";
+                    itemEditText.setText(emptyString);
                     items.add(item);
                     adapter.notifyDataSetChanged();
 
@@ -46,6 +63,28 @@ public class MainActivity extends AppCompatActivity implements UpdateDialogFragm
                 updateFragmentDialog(i);
             }
         });
+
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l){
+                enableDeletionView();
+                return true;
+            }
+        });
+
+        acceptDeletionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO eliminate items selected
+            }
+        });
+
+        cancelDeletionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                disableDeletionView();
+            }
+        });
     }
 
     private void updateFragmentDialog(int i){
@@ -53,7 +92,22 @@ public class MainActivity extends AppCompatActivity implements UpdateDialogFragm
         UpdateDialogFragment dialogFragment = UpdateDialogFragment.newInstance(
                 items.get(i), i
         );
+
         dialogFragment.show(manager, getString(R.string.title_dialog_fragment));
+    }
+
+    private void enableDeletionView(){
+        deleteButtonsLayout.setVisibility(View.VISIBLE);
+        addItemLayout.setVisibility(View.GONE);
+        adapter.setDeletable(true);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void disableDeletionView(){
+        deleteButtonsLayout.setVisibility(View.GONE);
+        addItemLayout.setVisibility(View.VISIBLE);
+        adapter.setDeletable(false);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
